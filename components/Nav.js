51bf22e4f8ca11f30346ae990e5c3b2a0eac818c
Roshanak <roshanakjamali@gmail.com>
2019-12-23 22:21:35 +0330

    client side graphal

---
+++ components/Nav.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
+
+const Nav = props => (
+  <NavStyles>
+    <Link href="/items">
+      <a>shop</a>
+    </Link>
+    <Link href="/sell">
+      <a>sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
